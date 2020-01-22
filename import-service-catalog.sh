37923b0d80331113c5406ee056d6ae3a29c86864
Jonathan Leek <jonathan@byteable.io>
2020-01-22 07:40:21 -0600

    Including for posterity. TODO: remove unecesary or unused components.

--- import-service-catalog.sh
+++ import-service-catalog.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-API_ENDPOINT="https://3scale-admin.apps.experian.demo.readyhat.guru/admin/api/services.xml"
+API_ENDPOINT="https://3scale-admin.apps.experian.demo.readyhat.guru/admin/api/services"
 API_ACCESS_TOKEN="dd8d3852c7da85f59ec8b47a6563f7d5bd844ce28609444ee67c6495ced5399c"
 
 # 3scale Service Import 
@@ -9,30 +9,25 @@ API_ACCESS_TOKEN="dd8d3852c7da85f59ec8b47a6563f7d5bd844ce28609444ee67c6495ced539
 #
 # @service_name
 # @swagger_spec path
-3scale_import () {
-
-  local system_name_check=$(3scale service list catalog | tail -n +2 | awk '{print $3}' | grep $1)
-
-  # if [ -z "${system_name_check/ /}" ]; then
-  #   echo "Service system_name ($1) already exists. Exiting."
-  #   return;
-  # fi
+importOpenApiSpec () {
 
   # Begin import
-  echo "Importing $1 ($2) to catalog via 3scale-toolbox..."
+  echo "Importing OpenAPI Specification $1 ($2)..."
   3scale import openapi \
     --destination catalog $2 \
     --override-private-base-url="https://echo-api.3scale.net" \
     --default-credentials-userkey=userkey \
-    --target_system_name $1
+    --target_system_name $1 
 
-  # TODO: Do we need to create a plan?
+  # Ceate a plan
   echo "Creating a default application plan..."
   3scale application-plan apply catalog $1 Default \
     --name "Default Plan" \
-    --default
+    --default \
+    --enabled \
+    --publish
 
-  echo "Create the application"    
+  echo "Creating the application..."    
   3scale application apply catalog $1 \
     --account admin+test@3scale.apps.experian.demo.readyhat.guru \
     --name "$1" \
@@ -40,19 +35,35 @@ API_ACCESS_TOKEN="dd8d3852c7da85f59ec8b47a6563f7d5bd844ce28609444ee67c6495ced539
     --plan Default \
     --service=$1
 
-  echo "Export staging URL"
-  STAGING_URL=$(3scale proxy-config show catalog $1 sandbox | jq -r ".content.proxy.sandbox_endpoint")
+  # local sandbox_endpoint=$(3scale proxy-config show catalog $1 sandbox | jq -r ".content.proxy.sandbox_endpoint")
 
-  # Readiness probe
-  # curl -D - -H "api-key: TODO: What does here" "$STAGING_URL/TODO: What endpoing are we testing here?"
+  local service_id=$(3scale service list catalog | grep $1 | awk '{print $1}') 
+  # Patch the 3scale adapter handler with the updated service id
+  oc patch handler petstore-handler \
+    --type='json' \
+    -p='[{"op": "replace", "path": "/spec/params/service_id", "value":"'$service_id\"'}]'
+
+  # Update deployment option to istio
+  curl -X PUT "https://3scale-admin.apps.experian.demo.readyhat.guru/admin/api/services/$service_id.xml" \
+    -d 'access_token=d85fa7da04f2324feb0a77e2eaf565ae27e5203b2d4a7ef13ed39c193b4556e9&deployment_option=service_mesh_istio' \
+    --silent --output /dev/null
+
+  # Enable access to service from developer portal
+  # local application_plan_id=$(3scale application-plan list catalog 47 | tail -n +2 | awk '{print $1}')
+  # curl -X POST "https://3scale-admin.apps.experian.demo.readyhat.guru/admin/api/account_plans/$application_plan_id/features
 
   echo "Promoting service to production..."
   3scale proxy-config promote catalog $1
+
+  local production_endpoint=$(3scale proxy-config show catalog $1 production | jq -r ".content.proxy.endpoint")
+  echo "Production endpoint: $production_endpoint/api/findByStatus?status=available,sold&user_key=$1"
 }
 
-# Playbook Import
-playbook_import () {
-  echo "Importing $1 to catalog via ansible-playbook..."
+# Import via 3scale API
+#
+# @service_name
+importVia3ScaleApi () {
+  echo "Importing service $1 via 3scale API..."
 
   local access_token=$API_ACCESS_TOKEN
   local name=$1
@@ -62,8 +73,42 @@ playbook_import () {
   local system_name=$name
 
   # Create the service
-  ansible-playbook playbooks/create-api-service.yaml \
-    --extra-vars "api_endpoint=$API_ENDPOINT service_name=$name body=access_token=$access_token&name=$name&description=$description&deployment_option=$deployment_option&backend_version=$backend_version&system_name=$system_name"
+  curl \
+    -H "Accept: */*" \
+    -H "Content-Type: application/x-www-form-urlencoded" \
+    -X POST "$API_ENDPOINT.xml" \
+    -d "access_token=$access_token&name=$name&description=$description&deployment_option=$deployment_option&backend_version=$backend_version&system_name=$system_name" \
+    --silent --output /dev/null
+
+  # Query the service id
+  local service_id=$(3scale service list catalog | grep $name | awk '{print $1}')
+
+  # Quert metric id
+  local metric_id=$(3scale metric list catalog $service_id | tail -n +2 | awk '{print $1}')
+
+  # Create the endpoint
+  curl \
+    -H "Accept: */*" \
+    -H "Content-Type: application/x-www-form-urlencoded" \
+    -X POST "$API_ENDPOINT/$service_id/proxy/mapping_rules.xml" \
+    -d "access_token=$access_token&http_method=GET&pattern=/&delta=1&metric_id=$metric_id" \
+    --silent --output /dev/null
+
+  # Ceate a plan
+  echo "Creating a default application plan..."
+  3scale application-plan apply catalog $1 Default \
+    --name "Default Plan" \
+    --default \
+    --enabled \
+    --publish
+
+  echo "Creating the application..."    
+  3scale application apply catalog $1 \
+    --account admin+test@3scale.apps.experian.demo.readyhat.guru \
+    --name "$1" \
+    --description "Imported service." \
+    --plan Default \
+    --service=$1
 }
 
 # TODO: Describe
@@ -74,19 +119,20 @@ SERVICE_MESH_CONFIGURED_MEMBERS=$(oc get ServiceMeshMemberRoll default --namespa
 for service_name in $SERVICE_MESH_CONFIGURED_MEMBERS; do
 
   # Get the VS endpoint and remove quotes
-  echo "Discovery hosts for $service_name service..."
+  echo "Discovering hosts for $service_name service..."
   virtual_service=$(oc get virtualservice --namespace $service_name --output json | jq '.items[0].spec.hosts[0]' | sed -e 's/^"//' -e 's/"$//');
 
+  # Swagger spec
   swagger_spec=http://$virtual_service/api/swagger.json
 
   # Evaluate swagger access
   if [ $(curl -s -o /dev/null -w "%{http_code}" $swagger_spec) = "200" ]; then
-    echo "Valid API Spec for: $service_name!"
-    3scale_import $service_name $swagger_spec
+    echo "Valid API Spec for $service_name!"
+    importOpenApiSpec $service_name $swagger_spec
   else 
     # TODO: Need to do the manual creation of service and backend
     echo "Invalid API Spec for: $service_name!"
-    # playbook_import $service_name
+    # importVia3ScaleApi $service_name
   fi
 done;
 
